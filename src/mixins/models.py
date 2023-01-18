4e2b7f78b2a50b01ea5fbb517d0e55c7d1d9a125
flavien-hugs <geekactux@gmail.com>
2023-01-18 23:30:43 +0000

    Update user models

--- src/mixins/models.py
+++ src/mixins/models.py
@@ -1,10 +1,9 @@
 import uuid
-from datetime import datetime
 
+from datetime import datetime
 
 from .. import db
 from ..utils import Updateable
-from sqlalchemy_utils import UUIDType, IPAddressType
 
 
 class TimestampMixin(Updateable, db.Model):
@@ -12,8 +11,8 @@ class TimestampMixin(Updateable, db.Model):
     __abstract__ = True
 
     id = db.Column(
-        UUIDType(binary=False), index=True,
-        primary_key=True, default=uuid.uuid4
+        db.String(40), unique=True, index=True,
+        nullable=False, primary_key=True, default=str(uuid.uuid4())
     )
     vn_created_at = db.Column(db.DateTime, default=datetime.utcnow())
     vn_updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow())
