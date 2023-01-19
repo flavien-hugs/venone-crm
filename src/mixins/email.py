da98b044a1874ec14df0ad075a071e3973ceae11
flavien-hugs <geekactux@gmail.com>
2023-01-19 20:25:17 +0000

    Edit auth design template

--- src/mixins/email.py
+++ src/mixins/email.py
@@ -1,6 +1,7 @@
 from threading import Thread
 
-from flask import current_app, render_template
+from flask import current_app
+from flask import render_template
 from flask_mail import Message
 
 from .. import mail
@@ -9,14 +10,15 @@ from .. import mail
 def send_async_email(app, to, subject, template, **kwargs):
     with app.app_context():
         msg = Message(subject, recipients=[to])
-        msg.body = render_template(template + '.txt', **kwargs)
-        msg.html = render_template(template + '.html', **kwargs)
+        msg.body = render_template(template + ".txt", **kwargs)
+        msg.html = render_template(template + ".html", **kwargs)
         mail.send(msg)
 
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
     thr = Thread(
-        target=send_async_email, args=(app, to, subject, template), kwargs=kwargs)
+        target=send_async_email, args=(app, to, subject, template), kwargs=kwargs
+    )
     thr.start()
     return thr
