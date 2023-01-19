0a8d6118c934e031213e535ed9189237154b4c07
flavien-hugs <geekactux@gmail.com>
2023-01-19 20:24:33 +0000

    Edit models mixins

--- src/mixins/models.py
+++ src/mixins/models.py
@@ -1,5 +1,4 @@
 import uuid
-
 from datetime import datetime
 
 from .. import db
@@ -11,8 +10,12 @@ class TimestampMixin(Updateable, db.Model):
     __abstract__ = True
 
     id = db.Column(
-        db.String(40), unique=True, index=True,
-        nullable=False, primary_key=True, default=str(uuid.uuid4())
+        db.String(40),
+        unique=True,
+        index=True,
+        nullable=False,
+        primary_key=True,
+        default=str(uuid.uuid4()),
     )
     vn_created_at = db.Column(db.DateTime, default=datetime.utcnow())
     vn_updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow())
