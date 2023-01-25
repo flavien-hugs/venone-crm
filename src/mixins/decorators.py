9f79626a6b8d8e2b326bd919d455f3cf15403701
flavien-hugs <geekactux@gmail.com>
2023-01-25 16:15:34 +0000

    Edit decorators for admin

--- src/mixins/decorators.py
+++ src/mixins/decorators.py
@@ -1,22 +1,21 @@
 from functools import wraps
 
-from flask import abort
+from flask import flash, abort
 from flask_login import current_user
 from src.auth.models import Permission
 
 
 def permission_required(permission):
-    def decorator(f):
-        @wraps(f)
+    def decorator(func):
+        @wraps(func)
         def decorated_function(*args, **kwargs):
             if not current_user.can(permission):
+                flash("Vous n'êtes pas autorisé à accéder à cette page.", category='warning')
                 abort(403)
-            return f(*args, **kwargs)
-
+            return func(*args, **kwargs)
         return decorated_function
-
     return decorator
 
 
-def admin_required(f):
-    return permission_required(Permission.ADMIN)(f)
+def admin_required(func):
+    return permission_required(Permission.ADMIN)(func)
