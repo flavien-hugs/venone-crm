896bda8a566b0e2a4db3b2372248da9c1809ddf4
flavien-hugs <geekactux@gmail.com>
2023-01-25 16:15:13 +0000

    Edit admin dashboard routes

--- src/auth/routes/dashboard.py
+++ src/auth/routes/dashboard.py
@@ -4,15 +4,26 @@ from flask_login import current_user
 from flask_login import login_required
 
 
-dashboard_view = Blueprint("dashboard_view", __name__)
+dashboard_view = Blueprint("dashboard_view", __name__, url_prefix="/customer/dashboard/")
 
 
-@dashboard_view.route("/dashboard/", methods=["GET"])
+@dashboard_view.route("/api/", methods=["GET"])
+@login_required
+def api():
+    from flask import jsonify
+    data = {
+        "id": current_user.id,
+        "addr_email": current_user.vn_user_addr_email
+    }
+    return jsonify({"data": data}), 200
+
+
+@dashboard_view.route("/index/", methods=["GET"])
 @login_required
 def dashboard():
     page_title = "Tableau de board"
     return render_template(
         "auth/admin/dashboard.html",
         page_title=page_title,
-        current_user=current_user,
+        current_user=current_user
     )
