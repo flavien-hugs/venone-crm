8b13a914e1a3bfef539a9e89eae20075f33b8569
flavien-hugs <geekactux@gmail.com>
2023-02-04 15:10:14 +0000

    define others pages resources

---
+++ src/main/errors.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, jsonify
+
+error_bp = Blueprint("error_bp", __name__, url_prefix="/error/")
+
+
+@error_bp.app_errorhandler(403)
+def forbidden(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
+    return render_template('pages/error.html'), 403
+
+
+@error_bp.app_errorhandler(404)
+def page_not_found(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
+    return render_template('errors/error.html'), 404
+
+
+@error_bp.app_errorhandler(500)
+def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
+    return render_template('errors/error.html'), 500
