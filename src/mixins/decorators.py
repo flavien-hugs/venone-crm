bd1add85b67ce3032a980dcf27497aa0ff321c34
flavien-hugs <geekactux@gmail.com>
2023-02-04 15:14:50 +0000

    edit decorators

--- src/mixins/decorators.py
+++ src/mixins/decorators.py
@@ -2,6 +2,7 @@ from functools import wraps
 
 from flask import abort
 from flask import flash
+from flask import render_template
 from flask_login import current_user
 from src.auth.models import Permission
 
@@ -25,3 +26,32 @@ def permission_required(permission):
 
 def admin_required(func):
     return permission_required(Permission.ADMIN)(func)
+
+
+def check_activated(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if current_user.vn_user_activated is False:
+            flash('Désolé, votre compte a été désactivé !', category='warning')
+            return redirect(url_for('auth_bp.unactivated'))
+        return func(*args, **kwargs)
+
+    return decorated_function
+
+
+def owner_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if current_user.vn_user_house_owner:
+            return func(*args, **kwargs)
+        return render_template("pages/no_access.html", page_title="Accès non autorisé")
+    return decorated_function
+
+
+def agency_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if current_user.vn_user_company:
+            return func(*args, **kwargs)
+        return render_template("pages/no_access.html", page_title="Accès non autorisé")
+    return decorated_function
