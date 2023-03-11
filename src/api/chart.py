c8675d59c0f226430965c21f17e1f7dbf53d4cff
BEF <geekactux@gmail.com>
2023-03-11 14:13:12 +0000

    update data to chart

--- src/api/chart.py
+++ src/api/chart.py
@@ -9,21 +9,21 @@ from . import api
 @login_required
 def get_owners_data():
     data = VNUser.get_ownerbymonth()
-    return jsonify([{"month": d[0], "count": d[1]} for d in data])
+    return jsonify([{"year": d[0], "month": d[1], "count": d[2]} for d in data])
 
 
 @api.get("/tenants/data/")
 @login_required
 def get_tenants_data():
     data = VNUser.get_tenantbymonth()
-    return jsonify([{"month": d[0], "count": d[1]} for d in data])
+    return jsonify([{"year": d[0], "month": d[1], "count": d[2]} for d in data])
 
 
 @api.get("/trendprices/data/")
 @login_required
 def get_trendprices_data():
     data = VNUser.get_trendprices()
-    return jsonify([{"month": d[0], "price": d[1]} for d in data])
+    return jsonify([{"year": d[0], "month": d[1], "price": d[2]} for d in data])
 
 
 @api.get("/available_properties/data/")
