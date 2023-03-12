7b2c09bbdc16f3d6bda30f6d5002ee63261df7c5
BEF <geekactux@gmail.com>
2023-03-12 16:48:42 +0000

    update api endpoint

--- src/api/payment.py
+++ src/api/payment.py
@@ -1,10 +1,5 @@
 from flask import jsonify
-from flask import request
-from flask import url_for
-from flask_login import current_user
 from flask_login import login_required
-
-from src import db
 from src.payment import VNPayment
 
 from . import api
