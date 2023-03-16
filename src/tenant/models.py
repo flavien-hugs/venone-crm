fa997df5ddbeebda62d512f0ffd4b3299e5c652e
BEF <geekactux@gmail.com>
2023-03-16 11:36:48 +0000

    update tenant and add task

--- src/tenant/models.py
+++ src/tenant/models.py
@@ -1,11 +1,14 @@
 import random
 import string
 from datetime import date
+from datetime import datetime
+from datetime import timedelta
 
 from flask_login import current_user
 from src import db
 from src.mixins.models import DefaultUserInfoModel
 from src.mixins.models import TimestampMixin
+from src.payment import VNPayment
 
 
 def id_generator():
@@ -68,7 +71,7 @@ class VNHouseOwner(DefaultUserInfoModel, TimestampMixin):
             "number_tenants": self.tenants.count(),
             "number_payments": self.payments.count(),
             "is_activated": self.vn_activated,
-            "created_at": self.vn_created_at.strftime("%d-%m-%Y"),
+            "created_at": self.vn_created_at.isoformat(),
         }
         return json_owner
 
@@ -191,6 +194,7 @@ class VNHouse(TimestampMixin):
     )
 
     def to_json(self):
+        lease_end_date = VNHouse.update_expired_lease_end_dates()
         json_house = {
             "house_uuid": self.uuid,
             "owner_id": self.get_owner_id(),
@@ -206,15 +210,27 @@ class VNHouse(TimestampMixin):
             "house_address": self.vn_house_address,
             "house_is_open": self.vn_house_is_open,
             "house_status": self.get_house_open(),
+            "status_payment": self.get_status_payment(),
             "house_lease_start_date": self.vn_house_lease_start_date.strftime(
                 "%d-%m-%Y"
             ),
-            "house_lease_end_date": self.vn_house_lease_end_date.strftime("%d-%m-%Y"),
+            "house_lease_end_date": lease_end_date.strftime("%d-%m-%Y")
+            if lease_end_date
+            else self.vn_house_lease_end_date.strftime("%d-%m-%Y"),
         }
         return json_house
 
     def __str__(self):
         return self.vn_house_type
+
+    def __getattr__(self, name):
+        """
+        Appeler update_expired_lease_end_dates()
+        si l'attribut demandé n'est pas trouvé
+        """
+        if name != "update_expired_lease_end_date()":
+            self.update_expired_lease_end_date()
+        return super().__getattr__(name)
 
     def __repr__(self):
         return f"VNHouse({self.id}, {self.vn_house_type})"
@@ -223,6 +239,9 @@ class VNHouse(TimestampMixin):
         self.vn_house_is_open = False
         db.session.add(self)
         db.session.commit()
+
+    def get_status_payment(self) -> bool:
+        return "payé" if self.is_rent_paid() else "impayé"
 
     def get_house_open(self) -> bool:
         return "indisponible" if self.vn_house_is_open else "disponible"
@@ -246,12 +265,154 @@ class VNHouse(TimestampMixin):
         )
         return next((own.get_owner_id() for own in owners), None)
 
+    def get_house_tenants(self):
+        """
+        liste des locataires de la propriété
+        """
+        house = VNHouse.query.filter_by(
+            vn_user_id=current_user.id, vn_house_id=self
+        ).first()
+        return house.tenants
+
     def get_current_tenant(self):
-        house = VNHouse.query.filter_by(id=self.id, vn_user_id=current_user.id).first()
+        house = VNHouse.query.filter_by(id=self.id).first()
         for tenant in house.tenants:
             if tenant is None:
                 return None
-            return tenant.get_tenant_id()
+            return tenant.vn_fullname
+
+    def get_tenant_phone_number(self):
+        house = VNHouse.query.filter_by(id=self.id).first()
+        for tenant in house.tenants:
+            if tenant is None:
+                return None
+            return tenant.vn_phonenumber_one
+
+    def is_rent_paid(tenant):
+        """
+        vérifie si le loyer du mois en cours
+        avant la date d'échéance a été effectué ou pas
+        """
+
+        current_date = datetime.utcnow().date()
+        current_year = current_date.year
+        current_month = current_date.month
+
+        last_payment = (
+            VNHouse.query.join(VNPayment, VNHouse.id == VNPayment.vn_house_id)
+            .filter(
+                VNPayment.id == tenant.id,
+                db.extract("month", VNPayment.vn_pay_date) < current_month,
+                db.extract("year", VNPayment.vn_pay_date) < current_year,
+            )
+            .first()
+        )
+
+        return last_payment is not None and last_payment.payment_status
+
+    def update_lease_end_date(self):
+        today = date.today()
+        notice_period = timedelta(days=15)
+
+        if today > self.vn_house_lease_end_date:
+            next_month = (
+                self.vn_house_lease_end_date.replace(day=28)
+                + timedelta(days=45)
+                - notice_period
+            )
+            self.vn_house_lease_end_date = next_month.replace(
+                day=min(self.vn_house_lease_start_date.day, next_month.day)
+            )
+            db.session.add(self)
+            db.session.commit()
+
+    @staticmethod
+    def update_expired_lease_end_dates():
+
+        today = date.today()
+
+        expired_houses = VNHouse.query.filter(
+            VNHouse.vn_house_is_open is True, VNHouse.vn_house_lease_end_date <= today
+        ).all()
+
+        for house in expired_houses:
+            house.update_lease_end_date()
+        db.session.commit()
+
+    def get_missed_payments(self):
+        today = date.today()
+        if self.vn_house_lease_end_date < today:
+            missed_payments = []
+            for paid in self.payments:
+                if paid.vn_pay_date > self.vn_house_lease_end_date:
+                    missed_payments.append(paid)
+            return missed_payments
+        else:
+            return []
+
+    @staticmethod
+    def tenants_paids():
+
+        available_houses = VNHouse.query.filter(
+            VNHouse.vn_house_is_open is True,
+            VNHouse.vn_house_lease_end_date >= date.today(),
+            VNHouse.vn_user_id == current_user.id,
+        ).all()
+
+        payments = [
+            VNPayment.query.filter(
+                VNPayment.vn_house_id == house.vn_house_id,
+                VNPayment.vn_pay_date < house.vn_house_lease_end_date,
+                VNPayment.vn_payee_id == current_user.id,
+            ).all()
+            for house in available_houses
+        ]
+
+        return payments
+
+    @staticmethod
+    def tenants_not_paid():
+        """
+        récupérer les locataires qui n'ont pas
+        effectué de paiement pour le mois en cours
+        """
+
+        from src.auth.models import VNUser
+
+        today = datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+        not_paid = (
+            db.session.query(VNTenant)
+            .join(VNPayment)
+            .join(VNUser)
+            .filter(VNTenant.vn_user_id == current_user.id)
+            .filter(VNPayment.vn_pay_date < today)
+            .filter(db.and_(VNPayment.vn_pay_date != VNHouse.vn_house_lease_start_date))
+            .all()
+        )
+        return not_paid
+
+    @staticmethod
+    def tenants_paid():
+        """
+        récupérer les locataires qui ont
+        effectué un paiement pour le mois en cours
+        """
+
+        current_month = date.today().month
+        current_year = date.today().year
+
+        paid = (
+            db.session.query(VNTenant)
+            .join(VNTenant.payments)
+            .filter(
+                VNTenant.vn_user_id == current_user.id,
+                db.extract("month", VNPayment.vn_pay_date) == current_month,
+                db.extract("year", VNPayment.vn_pay_date) == current_year,
+            )
+            .all()
+        )
+        return paid
 
 
 class VNTenant(DefaultUserInfoModel, TimestampMixin):
@@ -306,7 +467,7 @@ class VNTenant(DefaultUserInfoModel, TimestampMixin):
         return json_tenant
 
     def __str__(self):
-        return f"{self.vn_gender} {self.vn_fullname}"
+        return f"{self.id} {self.vn_fullname}"
 
     def __repr__(self):
         return f"VNTenant({self.id}, {self.vn_tenant_id})"
@@ -337,8 +498,6 @@ class VNTenant(DefaultUserInfoModel, TimestampMixin):
         return tenant
 
     def get_monthly_rent(self) -> float:
-
-        from src.payment import VNPayment
 
         today = date.today()
         mouth_rent = self.house_tenant.vn_house_rent
---
+++ src/tenant/tasks.py
@@ -0,0 +1,24 @@
+import os
+import requests
+from datetime import date, timedelta
+
+from flask import current_app
+
+from celery import shared_task
+
+from src.payment import VNPayment
+from src.tenant import VNHouse, VNTenant
+from src.main.routes import send_sms_reminder
+
+
+@shared_task
+def send_payment_reminders():
+    """
+    Envoie un email de rappel de paiement à tous les locataires pour chaque propriété
+    """
+
+    houses = VNHouse.query.filter_by(vn_house_is_open=True).all()
+
+    for house in houses:
+        for tenant in house.tenants:
+            send_sms_reminder(house, tenant)
