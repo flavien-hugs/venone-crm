1f8b2bf22535353dbc49fd62ff681a7d97a1960b
BEF <geekactux@gmail.com>
2023-03-19 16:37:12 +0000

    add features to export data

---
+++ src/dashboard/services/export_data.py
@@ -0,0 +1,50 @@
+
+import time
+import csv
+import io
+import requests
+import schedule
+from flask import Response
+
+def generate_tenant_csv(data, headers):
+
+    output = io.StringIO()
+    writer = csv.writer(output)
+
+    writer.writerow(headers)
+
+    for row in data:
+        writer.writerow([
+            row.vn_tenant_id,
+            row.vn_fullname,
+            row.vn_phonenumber_one,
+            row.vn_addr_email,
+            row.vn_cni_number,
+            row.vn_profession,
+            row.house_tenant.vn_house_rent,
+            row.vn_created_at,
+        ])
+
+    return output.getvalue()
+
+
+def generate_owner_csv(data, headers):
+
+    output = io.StringIO()
+    writer = csv.writer(output)
+
+    writer.writerow(headers)
+
+    for row in data:
+        writer.writerow([
+            row.vn_owner_id,
+            row.vn_fullname,
+            row.vn_phonenumber_one,
+            row.vn_addr_email,
+            row.vn_cni_number,
+            row.vn_profession,
+            row.houses.count(),
+            row.vn_created_at,
+        ])
+
+    return output.getvalue()
