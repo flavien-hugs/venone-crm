aff1b2d0d6d8ba9c2169b062d50f3fde22c763c9
BEF <geekactux@gmail.com>
2023-03-22 23:33:09 +0000

    update task

--- src/tenant/tasks.py
+++ src/tenant/tasks.py
@@ -1,16 +1,21 @@
+from datetime import datetime
+from datetime import timedelta
+
 from celery import shared_task
 from src.main.routes import send_sms_reminder
+from src.mixins.email import send_email_reminder
 from src.tenant import VNHouse
 
 
 @shared_task
-def send_payment_reminders():
-    """
-    Envoie un email de rappel de paiement à tous les locataires pour chaque propriété
-    """
+def payment_reminders():
 
+    current_date = datetime.utcnow().date()
     houses = VNHouse.query.filter_by(vn_house_is_open=True).all()
 
     for house in houses:
-        tenant = house.get_current_tenant()
-        send_sms_reminder(house, tenant)
+        reminder_date = house.vn_house_lease_end_date - timedelta(days=7)
+        if current_date <= reminder_date:
+            for tenant in house.tenants:
+                send_sms_reminder(house, tenant)
+                send_email_reminder(tenant, house)
