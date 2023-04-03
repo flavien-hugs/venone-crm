56e2b4c43ffd46cdba790ee7f56e6383bd16eba8
BEF <geekactux@gmail.com>
2023-04-03 12:22:57 +0000

    add tests

---
+++ tests/base_case.py
@@ -0,0 +1,34 @@
+import sys
+import unittest
+
+
+from flask import current_app
+
+from src import db
+from src import create_venone_app
+
+sys.path.append("..")
+
+
+class BaseCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_venone_app("test")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        self.assertTrue(current_app.config["TESTING"])
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_app_routes.py
@@ -0,0 +1,9 @@
+from unittest.mock import patch
+from tests.base_case import BaseCase
+
+
+class TestApp(BaseCase):
+    @patch("requests.get")
+    def test_map_view(self, mock_get):
+        response = self.client.get("/api/tenants/")
+        self.assertEqual(response.status_code, 200)
