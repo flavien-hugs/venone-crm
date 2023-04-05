363d78457c32fd0c360b157757ff3cb2a3596dc4
BEF <geekactux@gmail.com>
2023-04-05 14:01:29 +0000

    (feat) update payment models

--- src/payment/models.py
+++ src/payment/models.py
@@ -1,15 +1,15 @@
-import random
-import string
+import logging
 from datetime import date
-from decimal import Decimal
 
+from cinetpay_sdk.s_d_k import Cinetpay
+from flask import current_app
 from flask_login import current_user
 from src import db
+from src.mixins.models import id_generator
 from src.mixins.models import TimestampMixin
 
-
-def id_generator():
-    return "".join(random.choices(string.digits, k=5))
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 class VNPayment(TimestampMixin):
@@ -30,6 +30,7 @@ class VNPayment(TimestampMixin):
     vn_pay_late_penalty = db.Column(db.Float, nullable=True)
     vn_pay_date = db.Column(db.Date, nullable=False)
     vn_pay_status = db.Column(db.Boolean, default=False)
+    vn_cinetpay_data = db.Column(db.JSON, default=False)
 
     vn_payee_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     vn_owner_id = db.Column(db.Integer, db.ForeignKey("houseowner.id"))
@@ -40,17 +41,21 @@ class VNPayment(TimestampMixin):
         json_payment = {
             "payment_uuid": self.uuid,
             "user_id": self.vn_payee_id,
+            "owner_id": self.vn_owner_id,
+            "tenant_id": self.vn_tenant_id,
+            "house_id": self.vn_house_id,
             "payment_id": self.vn_payment_id,
             "trans_id": self.vn_transaction_id,
-            "house": self.house_payment.to_json(),
             "house_rent": self.vn_pay_amount,
+            "house": self.house_payment.to_json(),
+            "trans_info": self.vn_cinetpay_data,
             "amount_penalty": self.calculate_late_penalty(),
             "payment_date": self.vn_pay_date.strftime("%d-%m-%Y"),
             "payment_late_penalty": self.vn_pay_late_penalty,
             "payment_status": self.vn_pay_status,
             "get_payment": self.get_status_payment(),
-            "make_payment": self.make_payment(),
             "created_at": self.vn_created_at.strftime("%d-%m-%Y"),
+            "check_info_trans": self.check_transaction_trx(),
         }
         return json_payment
 
@@ -65,6 +70,32 @@ class VNPayment(TimestampMixin):
         payments = VNPayment.query.filter_by(vn_payee_id=current_user.id)
         return payments
 
+    def check_transaction_trx(self):
+        try:
+            app = current_app._get_current_object()
+            SITEID = app.config["CINETPAY_SITE_ID"]
+            APIKEY = app.config["CINETPAY_API_KEY"]
+            client = Cinetpay(APIKEY, SITEID)
+
+            response_data = client.TransactionVerfication_trx(self.vn_transaction_id)
+
+            if (
+                response_data["code"] == "00"
+                and response_data["data"]["status"] == "ACCEPTED"
+            ):
+                payment = VNPayment.query.filter_by(
+                    vn_transaction_id=self.vn_transaction_id
+                ).first()
+                if payment:
+                    payment.vn_pay_status = True
+                    payment.vn_cinetpay_data = response_data
+                    db.session.commit()
+        except Exception as e:
+            logger.warning(
+                f"Error verifying transaction with id {self.vn_transaction_id}: {e}"
+            )
+            
+
     def get_status_payment(self) -> bool:
         return "payé" if self.vn_pay_status else "impayé"
 
@@ -78,7 +109,3 @@ class VNPayment(TimestampMixin):
             db.session.commit()
         else:
             self.vn_pay_late_penalty = 0
-
-    def make_payment(self) -> bool:
-        self.vn_pay_status = True
-        db.session.commit()
