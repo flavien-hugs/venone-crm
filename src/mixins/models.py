58fc824103f63f83582ed1da2e29a4a0db214aac
BEF <geekactux@gmail.com>
2023-04-05 13:59:58 +0000

    (feat) update function generate id

--- src/mixins/models.py
+++ src/mixins/models.py
@@ -1,9 +1,13 @@
 import uuid
+import secrets
 from datetime import datetime
 
 from src import db
 from src.utils import Updateable
 
+
+def id_generator():
+    return secrets.randbelow(100000)
 
 class CRUDMixin(object):
 
@@ -20,7 +24,7 @@ class CRUDMixin(object):
     @classmethod
     def get_by_id(cls, id):
         if any(
-            (isinstance(id, basestring) and id.isdigit(), isinstance(id, (int, float))),
+            (isinstance(id, str) and id.isdigit(), isinstance(id, (int, float))),
         ):
             return cls.query.get(int(id))
         return None
