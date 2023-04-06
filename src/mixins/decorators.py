ec9b5493a6718fe39650861a214abe9665799032
BEF <geekactux@gmail.com>
2023-04-06 17:06:04 +0000

    (feat) update decorator owner and agency

--- src/mixins/decorators.py
+++ src/mixins/decorators.py
@@ -31,9 +31,8 @@ def owner_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if current_user.vn_house_owner and current_user.vn_activated:
-            return func(*args, **kwargs)
-        else:
-            abort(400)
+            abort(403)
+        return func(*args, **kwargs)
 
     return decorated_function
 
@@ -42,8 +41,7 @@ def agency_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if current_user.vn_company and current_user.vn_activated:
-            return func(*args, **kwargs)
-        else:
-            abort(400)
+            abort(403)
+        return func(*args, **kwargs)
 
     return decorated_function
