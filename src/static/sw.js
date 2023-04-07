26e6436664b6c2a2ddd5e4491cccf187da8d6014
BEF <geekactux@gmail.com>
2023-04-07 03:01:45 +0000

    (perf) update service worker function

--- src/static/sw.js
+++ src/static/sw.js
@@ -1,20 +1,45 @@
-self.addEventListener('install', function(e) {
+const CACHE_NAME = 'venone-cache'
+
+const urlsToCache = [
+    '/',
+    '/static/css/admin.css',
+    '/static/css/style.css',
+    '/static/js/admin.js'
+]
+
+self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open('venone').then(function(cache) {
-            return cache.addAll([
-                '/',
-            ]);
-        })
+        caches.open('CACHE_NAME')
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response
+                }
+                return fetch(event.request)
+            })
+    )
 
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName !== CACHE_NAME
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName)
+                })
+            );
+        })
+    )
 });
