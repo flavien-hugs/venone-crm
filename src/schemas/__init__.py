9335173188a4028d638ae4f49d92368fd71f3e2c
haf <haf@incubtek.com>
2023-04-22 17:13:53 +0000

    edit schema models

---
+++ src/schemas/__init__.py
@@ -0,0 +1,1 @@
+from .users import user_schema, users_schema  # noqa
---
+++ src/schemas/users.py
@@ -0,0 +1,18 @@
+from src import ma
+
+
+class UserSchema(ma.Schema):
+
+    class Meta:
+        fields = ("vn_addr_email", "vn_fullname", "_links")
+
+    _links = ma.Hyperlinks(
+        {
+            "self": ma.URLFor("api.user_detail", values=dict(id="<id>")),
+            "collection": ma.URLFor("api.users_all"),
+        }
+    )
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
