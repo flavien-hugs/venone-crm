67412dd743eb6f445ed310c0aae46ecdd7d51c49
haf <haf@incubtek.com>
2023-04-25 23:19:43 +0000

    add percent and transfer request models

--- runserver.py
+++ runserver.py
@@ -3,9 +3,11 @@ import logging as lg
 from flask_migrate import Migrate
 from flask_migrate import upgrade
 from src import db
+from src.auth.models import VNPercent
 from src.auth.models import VNRole
 from src.auth.models import VNUser
 from src.payment.models import VNPayment
+from src.payment.models import VNTransferRequest
 from src.tenant.models import VNHouse
 from src.tenant.models import VNHouseOwner
 from src.tenant.models import VNTenant
@@ -18,11 +20,13 @@ migrate = Migrate(venone_app, db, render_as_batch=True)
 def make_shell_context():
     return dict(
         db=db,
-        user=VNUser,
-        tenant=VNTenant,
-        house=VNHouse,
-        houseowner=VNHouseOwner,
-        payment=VNPayment,
+        users=VNUser,
+        percents=VNPercent,
+        tenants=VNTenant,
+        houses=VNHouse,
+        houseowners=VNHouseOwner,
+        payments=VNPayment,
+        transfers=VNTransferRequest,
     )
 
 
