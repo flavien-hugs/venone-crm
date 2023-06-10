9c9e185f02b4f1c1dd369dc3884eaa58fced83b7
haf <haf@incubtek.com>
2023-06-10 14:10:32 +0000

    update schemas models

--- src/schemas/houses.py
+++ src/schemas/houses.py
@@ -1,6 +1,9 @@
-from src import ma
-
+from marshmallow import fields
+from src.exts import ma
+from src.payment import VNPayment
+from src.payment import VNTransferRequest
 from src.tenant.models import VNHouse
+from src.tenant.models import VNTenant
 
 
 class VNHouseSchema(ma.SQLAlchemyAutoSchema):
@@ -8,8 +11,99 @@ class VNHouseSchema(ma.SQLAlchemyAutoSchema):
         model = VNHouse
         include_fk = True
         load_instance = True
-        include_relationships = True
+        exclude = ("id", "vn_user_id", "vn_owner_id")
+
+    devise = fields.Method("get_devise")
+    house_status = fields.Method("get_house_status")
+    owner = fields.Method("get_house_owner")
+    tenant = fields.Method("get_tenant")
+    house_percent = fields.Method("get_house_percent")
+    remaining_days = fields.Method("get_remaining_days")
+    vn_created_at = fields.DateTime(format="%d %B %Y")
+    vn_house_lease_end_date = fields.DateTime(format="%d %B %Y")
+    vn_house_lease_start_date = fields.DateTime(format="%d %B %Y")
+
+    def get_devise(self, obj):
+        return obj.user_houses.vn_device
+
+    def get_remaining_days(self, obj):
+        return obj.get_remaining_days()
+
+    def get_house_status(self, obj):
+        return obj.get_house_open()
+
+    def get_tenant(self, obj):
+        return obj.get_current_tenant()
+
+    def get_house_owner(self, obj):
+        return obj.get_house_owner()
+
+    def get_house_percent(self, obj):
+        return obj.get_house_rent_with_percent()
 
 
 house_schema = VNHouseSchema()
 houses_schema = VNHouseSchema(many=True)
+
+
+class VNTenantSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = VNTenant
+        include_fk = True
+        load_instance = True
+        exclude = ("id", "vn_user_id", "vn_owner_id")
+
+    owner = fields.Method("get_tenant_owner")
+    house = fields.Nested(house_schema)
+    vn_created_at = fields.DateTime(format="%d %B %Y")
+
+    def get_tenant_owner(self, obj):
+        return obj.get_tenant_owner()
+
+
+tenant_schema = VNTenantSchema()
+tenants_schema = VNTenantSchema(many=True)
+
+
+class VNPaymentSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = VNPayment
+        include_fk = True
+        load_instance = True
+        exclude = (
+            "id",
+            "vn_payee_id",
+            "vn_house_id",
+            "vn_owner_id",
+            "vn_tenant_id",
+        )
+
+    vn_created_at = fields.DateTime(format="%d %B %Y")
+    payment_status = fields.Method("get_payment_status")
+    check_info_trans = fields.Method("check_info_trans")
+    house = fields.Nested(house_schema)
+    tenant = fields.Nested(tenant_schema)
+
+    def check_info_trans(self, obj):
+        return obj.check_info_trans()
+
+    def get_payment_status(self, obj):
+        return obj.get_status_payment()
+
+
+payment_schema = VNPaymentSchema()
+payments_schema = VNPaymentSchema(many=True)
+
+
+class VNTransferRequestSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = VNTransferRequest
+        include_fk = True
+        load_instance = True
+        exclude = ("id", "vn_user_id")
+
+    vn_created_at = fields.DateTime(format="%d %B %Y")
+
+
+transfer_schema = VNTransferRequestSchema()
+transfers_schema = VNTransferRequestSchema(many=True)
--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,6 +1,18 @@
-from src import ma
+import locale
 
+from marshmallow import fields
 from src.auth.models import VNUser
+from src.exts import ma
+
+from .houses import houses_schema
+from .houses import payments_schema
+from .houses import tenants_schema
+from .houses import transfers_schema
+from .owners import owners_schema
+
+
+loc = locale.getlocale()
+locale.setlocale(locale.LC_ALL, loc)
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
@@ -9,6 +21,88 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
         include_fk = True
         load_instance = True
         include_relationships = True
+        exclude = (
+            "id",
+            "vn_password",
+        )
+
+    is_admin = fields.Method("is_admin")
+    vn_created_at = fields.DateTime(format="%d %B %Y")
+    owner_percent = fields.Method("get_owner_percent")
+    amount_apply_percent = fields.Method("amount_apply_percent")
+    company_percent = fields.Method("get_company_percent")
+    total_house_receive = fields.Method("total_houses_percent")
+    total_house_amount = fields.Method("total_houses_amount")
+    total_house_percent = fields.Method("get_amount_received")
+    payments_count = fields.Method("get_payments_count")
+    total_payment_month = fields.Method("total_payments_month")
+    total_payments_received = fields.Method("get_total_payments_received")
+    houses_count = fields.Method("get_houses_count")
+    houses_close_count = fields.Method("get_houses_close_count")
+    houses_open_count = fields.Method("get_houses_open_count")
+    tenants_count = fields.Method("get_tenants_count")
+    owners_count = fields.Method("get_houseowners_count")
+
+    houses = fields.Nested(houses_schema)
+    tenants = fields.Nested(tenants_schema)
+    houseowners = fields.Nested(owners_schema)
+    payments = fields.Nested(payments_schema)
+    transfers = fields.Nested(transfers_schema)
+
+    def is_admin(self, obj):
+        return obj.is_administrator()
+
+    def get_owner_percent(self, obj):
+        return obj.get_owner_percent()
+
+    def amount_apply_percent(self, obj):
+        return obj.amount_apply_percent()
+
+    def get_company_percent(self, obj):
+        return obj.get_company_percent()
+
+    def total_houses_percent(self, obj):
+        return locale.format_string(
+            "%.0f", obj.total_houses_percent(), grouping=True, monetary=True
+        )
+
+    def total_houses_amount(self, obj):
+        return locale.format_string(
+            "%.0f", obj.total_houses_amount(), grouping=True, monetary=True
+        )
+
+    def get_total_payments_received(self, obj):
+        return locale.format_string(
+            "%.0f", obj.get_total_payments_received(), grouping=True, monetary=True
+        )
+
+    def get_amount_received(self, obj):
+        return locale.format_string(
+            "%.0f", obj.get_amount_received(), grouping=True, monetary=True
+        )
+
+    def total_payments_month(self, obj):
+        return locale.format_string(
+            "%.0f", obj.total_payments_month(), grouping=True, monetary=True
+        )
+
+    def get_payments_count(self, obj):
+        return obj.get_payments_count()
+
+    def get_houses_count(self, obj):
+        return obj.get_houses_count()
+
+    def get_houses_close_count(self, obj):
+        return obj.get_houses_close_count()
+
+    def get_houses_open_count(self, obj):
+        return obj.get_houses_open_count()
+
+    def get_tenants_count(self, obj):
+        return obj.get_tenants_count()
+
+    def get_houseowners_count(self, obj):
+        return obj.get_houseowners_count()
 
 
 user_schema = UserSchema()
